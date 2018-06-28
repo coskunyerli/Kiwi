a9d49214579322a429e09d6ffd66cdc9fb3e5852
Coskun Yerli <gomez@cavevfx.com>
2018-06-28 11:29:57 +0300

    Added preferences dialogue and replace features.

--- python/MainWidget.py
+++ python/MainWidget.py
@@ -15,13 +15,13 @@ from PySide import QtCore, QtGui
 from HighLighting import HighlightingRule, Highlighter
 from python.models import FileListModel, FileListItem, StyleItem
 from python.path import filesPath
+from python.preferencesDialogue import Preferences
 
 class Ui_Form( object ):
     def setupUi( self, Form ):
         Form.setObjectName( "Form" )
-        # Form.resize( 868, 516 )
+        Form.resize( 868, 516 )
         self.horizontalLayout_3 = QtGui.QHBoxLayout( Form )
-        self.horizontalLayout_3.setSpacing( 3 )
         self.horizontalLayout_3.setObjectName( "horizontalLayout_3" )
         self.fileListWidget = QtGui.QWidget( Form )
         sizePolicy = QtGui.QSizePolicy( QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred )
@@ -61,26 +61,46 @@ class Ui_Form( object ):
         self.verticalLayout.setObjectName( "verticalLayout" )
         self.searchWidgetInEditor = QtGui.QWidget( self.editorWidget )
         self.searchWidgetInEditor.setObjectName( "searchWidgetInEditor" )
-        self.horizontalLayout = QtGui.QHBoxLayout( self.searchWidgetInEditor )
-        self.horizontalLayout.setContentsMargins( 0, 0, 0, 0 )
+        self.verticalLayout_3 = QtGui.QVBoxLayout( self.searchWidgetInEditor )
+        self.verticalLayout_3.setContentsMargins( 0, 0, 0, 0 )
+        self.verticalLayout_3.setObjectName( "verticalLayout_3" )
+        self.searchWidget = QtGui.QWidget( self.searchWidgetInEditor )
+        self.searchWidget.setObjectName( "searchWidget" )
+        self.horizontalLayout = QtGui.QHBoxLayout( self.searchWidget )
+        self.horizontalLayout.setContentsMargins( -1, 0, -1, 0 )
         self.horizontalLayout.setObjectName( "horizontalLayout" )
-        self.searchLineBoxInEditor = QtGui.QLineEdit( self.searchWidgetInEditor )
+        self.searchLineBoxInEditor = QtGui.QLineEdit( self.searchWidget )
         self.searchLineBoxInEditor.setObjectName( "searchLineBoxInEditor" )
-        self.searchLineBoxInEditor.setSizePolicy( sizePolicy )
         self.horizontalLayout.addWidget( self.searchLineBoxInEditor )
-        self.prevWordButton = QtGui.QPushButton( self.searchWidgetInEditor )
+        self.prevWordButton = QtGui.QPushButton( self.searchWidget )
         self.prevWordButton.setObjectName( "prevWordButton" )
-
         self.horizontalLayout.addWidget( self.prevWordButton )
-        self.nextWordButton = QtGui.QPushButton( self.searchWidgetInEditor )
+        self.nextWordButton = QtGui.QPushButton( self.searchWidget )
         self.nextWordButton.setObjectName( "nextWordButton" )
         self.horizontalLayout.addWidget( self.nextWordButton )
-        self.doneButton = QtGui.QPushButton( self.searchWidgetInEditor )
+        self.doneButton = QtGui.QPushButton( self.searchWidget )
         self.doneButton.setObjectName( "doneButton" )
         self.horizontalLayout.addWidget( self.doneButton )
-        self.replaceCheckBox = QtGui.QCheckBox( self.searchWidgetInEditor )
+        self.replaceCheckBox = QtGui.QCheckBox( self.searchWidget )
         self.replaceCheckBox.setObjectName( "replaceCheckBox" )
         self.horizontalLayout.addWidget( self.replaceCheckBox )
+        self.verticalLayout_3.addWidget( self.searchWidget )
+        self.replaceWidget = QtGui.QWidget( self.searchWidgetInEditor )
+        self.replaceWidget.setObjectName( "replaceWidget" )
+        self.horizontalLayout_4 = QtGui.QHBoxLayout( self.replaceWidget )
+        self.horizontalLayout_4.setContentsMargins( -1, 0, -1, 0 )
+        self.horizontalLayout_4.setObjectName( "horizontalLayout_4" )
+        self.replaceLineEdit = QtGui.QLineEdit( self.replaceWidget )
+        self.replaceLineEdit.setObjectName( "replaceLineEdit" )
+        self.replaceLineEdit.setPlaceholderText( 'Replace' )
+        self.horizontalLayout_4.addWidget( self.replaceLineEdit )
+        self.replaceButton = QtGui.QPushButton( self.replaceWidget )
+        self.replaceButton.setObjectName( "replaceButton" )
+        self.horizontalLayout_4.addWidget( self.replaceButton )
+        self.replaceAllButton = QtGui.QPushButton( self.replaceWidget )
+        self.replaceAllButton.setObjectName( "replaceAllButton" )
+        self.horizontalLayout_4.addWidget( self.replaceAllButton )
+        self.verticalLayout_3.addWidget( self.replaceWidget )
         self.verticalLayout.addWidget( self.searchWidgetInEditor )
         self.editor = QtGui.QTextEdit( self.editorWidget )
         self.editor.setObjectName( "editor" )
@@ -89,9 +109,6 @@ class Ui_Form( object ):
 
         self.retranslateUi( Form )
         QtCore.QMetaObject.connectSlotsByName( Form )
-        self.fileListView.setAttribute( QtCore.Qt.WA_MacShowFocusRect, False )
-        self.searchLineBoxInFileList.setAttribute( QtCore.Qt.WA_MacShowFocusRect, False )
-        self.searchLineBoxInEditor.setAttribute( QtCore.Qt.WA_MacShowFocusRect, False )
 
 
     def retranslateUi( self, Form ):
@@ -102,6 +119,10 @@ class Ui_Form( object ):
         self.doneButton.setText( QtGui.QApplication.translate( "Form", "Done", None, QtGui.QApplication.UnicodeUTF8 ) )
         self.replaceCheckBox.setText(
             QtGui.QApplication.translate( "Form", "Replace", None, QtGui.QApplication.UnicodeUTF8 ) )
+        self.replaceButton.setText(
+            QtGui.QApplication.translate( "Form", "Replace", None, QtGui.QApplication.UnicodeUTF8 ) )
+        self.replaceAllButton.setText(
+            QtGui.QApplication.translate( "Form", "All", None, QtGui.QApplication.UnicodeUTF8 ) )
 
 class MainWidget( Ui_Form, QtGui.QWidget ):
     def __init__( self, config, fileList, parent = None ):
@@ -118,6 +139,7 @@ class MainWidget( Ui_Form, QtGui.QWidget ):
 
 
     def initialize( self ):
+        self.fileListView.setAttribute( QtCore.Qt.WA_MacShowFocusRect, 0 )
         self.deleteItemShortCut = QtGui.QShortcut( self.fileListView )
         self.deleteItemShortCut.setContext( QtCore.Qt.WidgetShortcut )
         self.deleteItemShortCut.setKey( QtGui.QKeySequence( QtCore.Qt.Key_Backspace ) )
@@ -126,6 +148,10 @@ class MainWidget( Ui_Form, QtGui.QWidget ):
         self.reformatShortCut.setContext( QtCore.Qt.WidgetShortcut )
         self.reformatShortCut.setKey( QtGui.QKeySequence( 'Ctrl+Alt+K' ) )
         self.reformatShortCut.activated.connect( self.reformatBlocks )
+        self.quitSearchWordShortcut = QtGui.QShortcut( self.searchLineBoxInEditor )
+        self.quitSearchWordShortcut.setContext( QtCore.Qt.WidgetShortcut )
+        self.quitSearchWordShortcut.setKey( QtGui.QKeySequence( QtCore.Qt.Key_Escape ) )
+        self.quitSearchWordShortcut.activated.connect( self.hideSearchWidgetInEditor )
         self.fileListView.setModel( self.fileListModel )
         if self.fileList is None or len( self.fileList ) <= 0:
             self.newFile()
@@ -135,12 +161,14 @@ class MainWidget( Ui_Form, QtGui.QWidget ):
         self.searchLineBoxInEditor.setPlaceholderText( 'Search' )
         self.searchLineBoxInFileList.setPlaceholderText( 'Search' )
         self.searchWidgetInEditor.hide()
+        self.replaceWidget.hide()
         self.fileListWidget.setMaximumWidth( 200 )
         searchFormat = QtGui.QTextCharFormat()
         searchFormat.setBackground( QtGui.QColor( 'yellow' ) )
         self.rule = HighlightingRule( self.editor )
         self.rule.format = searchFormat
         self.highlighter.highlightingRules.append( self.rule )
+        self.rule.pattern = re.compile( '' )
         self.editor.setFocus()
         self.fileListView.setAcceptDrops( True )
         self.fileListView.setDragEnabled( True )
@@ -148,7 +176,7 @@ class MainWidget( Ui_Form, QtGui.QWidget ):
 
 
     def initSignalsAndSlots( self ):
-        self.doneButton.clicked.connect( self.searchWidgetInEditor.hide )
+        self.doneButton.clicked.connect( self.hideSearchWidgetInEditor )
         self.prevWordButton.clicked.connect( self.prevSearchText )
         self.nextWordButton.clicked.connect( self.nextSearchText )
         self.searchLineBoxInEditor.textChanged.connect( self.searchWord )
@@ -158,28 +186,105 @@ class MainWidget( Ui_Form, QtGui.QWidget ):
         self.fileListModel.dataUpdated.connect( self.titleNameChanged )
         self.timer.timeout.connect( self.saveFile )
         self.editor.document().contentsChanged.connect( self.contentChanged )
+        self.replaceCheckBox.stateChanged.connect(
+            lambda value: self.replaceWidget.show() if value else self.replaceWidget.hide() )
+        self.replaceAllButton.clicked.connect( self.replaceAllText )
+        self.replaceButton.clicked.connect( self.replaceText )
 
 
     def nextSearchText( self ):
         cursor = self.editor.textCursor()
-
         block = cursor.block()
+        text = block.text()[cursor.positionInBlock():]
         while block.isValid():
-            matches = self.rule.search( block.text() )
+            matches = self.rule.search( text )
             for match in matches:
-                cursor.setPosition( block.position() + match.start() )
+                cursor.setPosition( block.position() + match.end() )
                 self.editor.setTextCursor( cursor )
+                self.editor.setFocus()
                 return
+
             block = block.next()
+            text = block.text()
 
 
     def prevSearchText( self ):
-        pass
+        cursor = self.editor.textCursor()
+        block = cursor.block()
+        text = block.text()[0:cursor.positionInBlock()]
+        while block.isValid():
+            matches = self.rule.search( text )
+            for match in matches:
+                cursor.setPosition( block.position() + match.end() )
+                self.editor.setTextCursor( cursor )
+                self.editor.setFocus()
+                return
+
+            block = block.previous()
+            text = block.text()
+
+
+    def replaceAllText( self ):
+        oldWord = self.searchLineBoxInEditor.text()
+        if oldWord:
+            newWord = self.replaceLineEdit.text()
+            block = self.editor.document().firstBlock()
+            cursor = self.editor.textCursor()
+            cursor.beginEditBlock()
+            while block.isValid():
+                text = block.text()
+                if text.find( oldWord ) != -1:
+                    tcursor = QtGui.QTextCursor( block )
+                    tcursor.select( QtGui.QTextCursor.BlockUnderCursor )
+                    tcursor.removeSelectedText()
+                    tcursor.clearSelection()
+                    newText = text.replace( oldWord, newWord )
+                    tcursor.insertBlock()
+                    tcursor.insertText( newText )
+                block = block.next()
+            cursor.endEditBlock()
+
+
+    def replaceText( self ):
+        cursor = self.replaceNextText( self.editor.textCursor() )
+        self.editor.setTextCursor( cursor )
+
+
+    def replaceNextText( self, cursor ):
+        oldWord = self.searchLineBoxInEditor.text()
+        if oldWord:
+            newWord = self.replaceLineEdit.text()
+            cursor.beginEditBlock()
+            block = cursor.block()
+            while block.isValid():
+                text = block.text()
+                if text.find( oldWord ) != -1:
+                    tcursor = QtGui.QTextCursor( block )
+                    tcursor.select( QtGui.QTextCursor.BlockUnderCursor )
+                    tcursor.removeSelectedText()
+                    tcursor.clearSelection()
+                    newText = text.replace( oldWord, newWord )
+                    tcursor.insertBlock()
+                    tcursor.insertText( newText )
+                    return tcursor
+                block = block.next()
+            cursor.endEditBlock()
 
 
     def searchWord( self, text ):
         self.rule.pattern = re.compile( text )
-        self.editor.document().contentsChange.emit( 0, 0, 0 )
+        self.updateEditor()
+
+
+    def updateEditor( self ):
+        cursor = self.getFirstVisibleCursor()
+        block = cursor.block()
+        position = cursor.position()
+        firstPosition = position
+        while block.isValid() and block.isVisible():
+            position = block.position() + len( block.text() )
+            block = block.next()
+        self.editor.document().contentsChange.emit( firstPosition, position, position )
 
 
     def showSearch( self ):
@@ -204,6 +309,13 @@ class MainWidget( Ui_Form, QtGui.QWidget ):
     def titleNameChanged( self, old, new, index ):
         with open( 'fileList.json', 'w' ) as outfile:
             json.dump( self.fileListModel.json(), outfile )
+
+
+    def saveItemsStyle( self ):
+        with open( 'editor.conf', 'w' ) as outfile:
+            dict = {}
+            dict['patterns'] = map( lambda item: item.json(), self.styleItems )
+            json.dump( dict, outfile )
 
 
     def generateFileName( self ):
@@ -259,3 +371,26 @@ class MainWidget( Ui_Form, QtGui.QWidget ):
 
     def reformatBlocks( self ):
         print 'reformated'
+
+
+    def getFirstVisibleCursor( self ):
+        return self.editor.cursorForPosition( QtCore.QPoint( 0, 0 ) )
+
+
+    def hideSearchWidgetInEditor( self ):
+        self.searchWord( '' )
+        self.searchWidgetInEditor.hide()
+
+
+    def openPreferences( self ):
+        preferences = Preferences( self, preferences = self.styleItems )
+        preferences.acceptPreferences.connect( self.closePreferences )
+        preferences.open()
+
+
+    def closePreferences( self, patterns ):
+        self.styleItems = patterns
+        self.highlighter.updateHighlighterRules( patterns )
+        self.highlighter.highlightingRules.append( self.rule )
+        self.updateEditor()
+        self.saveItemsStyle()
