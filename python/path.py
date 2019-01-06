70af98c17b2cef92a640c15f1f5379f7fc8e0cb2
coskunyerli <coskun.yerli@gmail.com>
2019-01-06 16:31:55 +0300

    added path to absolute

--- python/path.py
+++ python/path.py
@@ -1,6 +1,6 @@
 import os
 
-projectPath = '.'
+projectPath = os.path.join( os.path.dirname( os.path.realpath( __file__ ) ), '..' )
 filePath = os.path.join( os.path.expanduser( '~' ), '.todolist' )
 editorPath = os.path.join( projectPath, 'editor.conf' )
 filesPath = os.path.join( filePath, 'files' )
