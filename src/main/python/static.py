3f8723918228f7c1740a2d748ae7a0368635c51f
coskunyerli <coskun.yerli@gmail.com>
2020-05-04 00:15:28 +0300

    first_ method is implemented

--- src/main/python/static.py
+++ src/main/python/static.py
@@ -21,3 +21,41 @@ def get_classes(name):
 		if inspect.isclass(obj):
 			klasses[obj.__name__] = obj
 	return klasses
+
+
+def first_(arr, func):
+	for item in arr:
+		if func(item):
+			return item
+	return None
+
+
+def _binarySearch(arr, l, r, cmp):
+	# Check base case
+	if r >= l:
+		mid = int(l + (r - l) / 2)
+		res = cmp(arr[mid])
+		# If element is present at the middle itself
+		if res == 0:
+			return mid
+
+		# If element is smaller than mid, then it
+		# can only be present in left subarray
+		elif res > 0:
+			return _binarySearch(arr, l, mid - 1, cmp)
+
+		# Else the element can only be present
+		# in right subarray
+		else:
+			return _binarySearch(arr, mid + 1, r, cmp)
+
+	else:
+		# Element is not present in the array
+		return None
+
+
+def binarySearch(arr, cmp):
+	return _binarySearch(arr, 0, len(arr) - 1, cmp)
+
+
+def cmp(a, b):    return (a > b) - (a < b)
