88c4115419c9d69f9f24158f9129969aebe3cb43
coskunyerli <coskun.yerli@gmail.com>
2020-05-04 00:12:13 +0300

    paths are defined in fbs object

--- src/main/python/model/fbs.py
+++ src/main/python/model/fbs.py
@@ -4,6 +4,15 @@ from fbs_runtime.application_context.PySide2 import ApplicationContext
 
 
 class TodoListApplicationContext(ApplicationContext):
+	def __init__(self):
+		super(TodoListApplicationContext, self).__init__()
+
+		if not os.path.isdir(self.filePath):
+			os.makedirs(self.filePath)
+		if not os.path.isdir(self.filesPath):
+			os.makedirs(self.filesPath)
+
+
 	def qss(self, filename):
 		try:
 			file = open(self.get_resource(os.path.join('qss', filename)))
@@ -20,3 +29,18 @@ class TodoListApplicationContext(ApplicationContext):
 	@property
 	def conf(self):
 		return self.get_resource('editor.conf')
+
+
+	@property
+	def filePath(self):
+		return os.path.join(os.path.expanduser('~'), '.todolist')
+
+
+	@property
+	def filesPath(self):
+		return os.path.join(self.filePath, 'files')
+
+
+	@property
+	def fileListPath(self):
+		return os.path.join(self.filePath, 'fileList.json')
